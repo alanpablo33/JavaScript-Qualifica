a811f27e27a1a2e0dd9b1a068096cd4ae0970cd4
alanpablo33 <alanpablo137@gmail.com>
2022-12-08 21:28:48 -0300

    08-12-2022

---
+++ Modulo-2/Boolean/Boolean.js
@@ -0,0 +1,17 @@
+//True (verdadeiro)
+//False (falso)
+
+console.log(5>2);
+console.log(3>10);
+console.log("\n")
+console.log(typeof true,false)
+console.log('\n\n')
+// comparações Boolean
+
+console.log(1<2);
+console.log(5>1);
+console.log(3>=3);
+console.log(3==3);
+console.log(3!=1);
+console.log(2==="2");
+console.log(3===3);
---
+++ Modulo-2/Operadores Logicos Teoria/OperadorLogico.js
@@ -0,0 +1,17 @@
+// && and se somente se
+// || or OU
+// ! not não
+
+console.log(true && true);
+console.log(true && false);
+console.log(false || false);
+console.log(!true);
+
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 2 || 3 == 1);
+
+console.log(3 == 3 && 'Alan' == 'Alan');
+console.log("Alan" == 'Pablo' || false);
+
+console.log(!(true && true));
+console.log(!(!(true && true)));
