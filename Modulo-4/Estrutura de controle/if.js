4508c2b7059f30e53e11063fa9093da13803931d
alanpablo33 <alanpablo137@gmail.com>
2022-12-10 10:58:54 -0300

    10/12/2022

---
+++ Modulo-4/Estrutura de controle/if.js
@@ -0,0 +1,16 @@
+//if e esle
+let idade = 10;
+
+if (idade> 18){
+console.log("Pode entrar")
+}
+else{
+console.log("Não pode entrar ")
+}
+
+if(idade == 20 && idade >18){
+console.log("Tudo OK")
+}
+else{
+    console.log("Não")
+}
